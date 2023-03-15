5ecf921571dad006a43e87dea367af24d9c769ed
WilcoApp <hello@wilco.gg>
2023-03-15 17:26:00 +0000

    Fixing A Bug

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -15,7 +15,6 @@ const mapStateToProps = (state) => ({
   ...state.editor,
 });
 
-
 const mapDispatchToProps = (dispatch) => ({
   onAddTag: () => dispatch({ type: ADD_TAG }),
   onLoad: (payload) => dispatch({ type: EDITOR_PAGE_LOADED, payload }),
@@ -56,7 +55,6 @@ class Editor extends React.Component {
         image: this.props.image,
         tagList: this.props.tagList,
       };
-
 
       const slug = { slug: this.props.itemSlug };
       const promise = this.props.itemSlug
